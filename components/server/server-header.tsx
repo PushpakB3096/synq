b97c94a32e9518a1f2e2046537af20f8d7acffc0
Pushpak Bhattacharya <rtpushpak@gmail.com>
2023-09-08 12:12:50 +0530

    feat: edit server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -51,7 +51,10 @@ const ServerHeader: React.FC<ServerHeaderProps> = ({ server, role }) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className='text-sm px-3 py-2 cursor-pointer'>
+          <DropdownMenuItem
+            className='text-sm px-3 py-2 cursor-pointer'
+            onClick={() => onOpen('editServer', { server })}
+          >
             Server Settings
             <Settings className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
